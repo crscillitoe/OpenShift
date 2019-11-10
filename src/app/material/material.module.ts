f7056509961d8b1ad6e4f080c33fb99b47be4a23
crscillitoe <crscillitoe@wpi.edu>
2019-11-10 10:23:30 -0500

    button for adding sites

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [],
   imports: [
-    MatIconModule,
     CommonModule
+  ],
+  exports: [
+    MatIconModule,
+    MatButtonModule
   ]
 })
 export class MaterialModule { }
